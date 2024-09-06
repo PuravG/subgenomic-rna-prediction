01fea84cf2b537e2e66898dc75e736ea9fd268c1
Purav Gupta <guptapurav@gmail.com>
2024-09-06 15:15:15 +0100

    Added aspera files

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 # path to reference virus
-virus_path='/drive/13xrrna/Rodent_hepacivirus.fasta'
+virus_path='/drive/13xrrna/Hepatitis_A_virus.fasta'
 
 # name of virus
-Virus='Rodent_hepacivirus'
+Virus='Hepatitis_A'
 Virus=$(tr -s ' ' '_' <<< $Virus)
 
 # path to a file with a list of accessions, one accession per line
-acc_list='/drive/13xrrna/hep_rodent_acc.txt'
+acc_list='/drive/13xrrna/Hepatitis_A_Virus_acc.txt'
 
 ./final_script1_new.sh $virus_path $Virus $acc_list
